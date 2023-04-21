e33e4ae03b232079593cbdc8ec132cc375f94b91
Rohit Dhamija <rohit.dhamija@gmail.com>
2023-04-21 23:50:53 +0530

    changed main.py file

--- main.py
+++ main.py
@@ -5,30 +5,23 @@ from langchain.llms import OpenAI
 template = """
     Below is an email that may be poorly worded.
     Your goal is to:
-    - Properly format the email
-    - Convert the input text to a specified tone
-    - Convert the input text to a specified dialect
-    Here are some examples different Tones:
-    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
-    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
-    Here are some examples of words in different dialects:
-    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
-    Example Sentences from each dialect:
-    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
-    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
-    Please start the email with a warm introduction. Add the introduction if you need to.
+    - Properly format the email  
+    - Convert the input text to a specified language
+
+    Here are some examples of words in different languages:
+    - English: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - Hindi: हम सप्ताहांत के लिए बार्सिलोना गए। हमें आपको बताने के लिए बहुत सी बातें हैं।
+    - Punjabi: ਅਸੀਂ ਵੀਕਐਂਡ ਲਈ ਬਾਰਸੀਲੋਨਾ ਗਏ ਸੀ। ਸਾਡੇ ਕੋਲ ਤੁਹਾਨੂੰ ਦੱਸਣ ਲਈ ਬਹੁਤ ਸਾਰੀਆਂ ਚੀਜ਼ਾਂ ਹਨ।
     
-    Below is the email, tone, and dialect:
-    TONE: {tone}
-    DIALECT: {dialect}
+    Below is the email, and language:
+    LANGUAGE: {language}
     EMAIL: {email}
     
-    YOUR {dialect} RESPONSE:
+    YOUR {language} RESPONSE:
 """
 
 prompt = PromptTemplate(
-    input_variables=["tone", "dialect", "email"],
+    input_variables=["language", "email"],
     template=template,
 )
 
@@ -45,12 +38,10 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+                will help you improve your email skills by converting your emails into a more professional format.")
 
 with col2:
-    st.image(image='test.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
+    st.image(image='test.png', width=500, caption='Powered by langchain, openai')
 
 st.markdown("## Enter Your Email To Convert")
 
@@ -60,16 +51,9 @@ def get_api_key():
 
 openai_api_key = get_api_key()
 
-col1, col2 = st.columns(2)
-with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
-    
-with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
+
+option_language = st.selectbox('Which language would you like to convert?',
+        ('English', 'Hindi', 'Punjabi'))
 
 def get_text():
     input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
@@ -96,7 +80,7 @@ if email_input:
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    prompt_with_email = prompt.format(language=option_language, email=email_input)
 
     formatted_email = llm(prompt_with_email)
 
